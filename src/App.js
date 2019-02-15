2cdf2d932f0bd3a5e7843ed29c3ae540c5531d6c
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-15 07:39:49 -0200

    CSS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './services/api';
 
 class App extends Component {
+
+  state = {
+    repositories: []
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault()
+
+    this.repository = document.querySelector('input[name=repository]').value
+
+    const response = await api.get(`/repos/${this.repository}`)
+
+    this.setState({ repositories: [...this.state.repositories, response.data] })
+
+    document.querySelector('input[name=repository]').value = ''
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>repository</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="repository" />
+          <button type="submit">Adicionar</button>
+        </form>
+        {this.state.repositories.map(repository => (
+          <div key={repository.id} className="git-list">
+            <h1>{repository.name}</h1>
+            <p>{repository.description}</p>
+            <img src={repository.owner.avatar_url} alt={repository.name}/>
+          </div>
+        ))}
       </div>
-    );
+    )
   }
 }
 
